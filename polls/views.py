f02b3c182fe5556c49f78841a127a1981d55a56c
Joanna Bien <joasiabien@gmail.com>
2022-06-04 21:31:37 +0200

    dodałam do admina mozliwość edycji i przeglądania obietków image, dodatkowo dodałam w katalogu views dodałam randomowe oznaczenia tworzonych obiektów zdjęcie

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
+import secrets
 import time
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from urllib.request import urlopen, Request
 
-from .models import Choice, Question
+from .models import Choice, Image, Question
 
 def detail(request, question_id):
     return HttpResponse("Czy ta osoba jest atrakcyjna? %s." % question_id)
@@ -24,8 +25,11 @@ def download(request):
     for n in x:
         requestImage = Request(url + '?' + str(n), headers={'User-Agent': 'Mozilla/5.0'})
         response = urlopen(requestImage)
-        with open('media/image-' + str(n) + '.jpg', "wb") as f:
+        fileName = 'image-' + secrets.token_hex(nbytes=8) + '.jpg'
+        with open('media/' + fileName, "wb") as f:
             f.write(response.read())
+        img = Image (file_name= fileName)
+        img.save()
         time.sleep(1)
 
     return HttpResponse("Tu ściągnęłam 5 zdjęć")
