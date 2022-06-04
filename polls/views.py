9a8d7067c40298f38ee7f975b94aa708a190ef77
Joanna Bien <joasiabien@gmail.com>
2022-06-04 19:01:32 +0200

    dodałam skrypy pobierające zdjęcia

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
+import time
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from urllib.request import urlopen, Request
 
 from .models import Choice, Question
 
@@ -11,7 +13,19 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
+    
 
 def download(request):
-    return HttpResponse("Tu będzie download")
+
+    ## konfiguruje adres URL obrazów
+    url = 'https://thispersondoesnotexist.com/image'
+
+    x = range(5)
+    for n in x:
+        requestImage = Request(url + '?' + str(n), headers={'User-Agent': 'Mozilla/5.0'})
+        response = urlopen(requestImage)
+        with open('media/image-' + str(n) + '.jpg', "wb") as f:
+            f.write(response.read())
+        time.sleep(1)
+
+    return HttpResponse("Tu ściągnęłam 5 zdjęć")
